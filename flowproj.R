1dbd9f5b677747d456661ccf203a020c79ce9987
greg-j-paul <gregjudepaul@gmail.com>
2017-01-14 23:25:59 +1100

    made some changes for package creation including Roxygen changes

--- flowproj.R
+++ flowproj.R
@@ -1,17 +1,31 @@
 
 
 
-edges = data.frame(N1 = paste0(rep(LETTERS[1:4], each = 4), rep(1:5, each = 16)),
-                   N2 = paste0(rep(LETTERS[1:4], 4), rep(2:6, each = 16)),
-                   Value = runif(80, min = 2, max = 5) * rep(c(1, 0.8, 0.6, 0.4, 0.3), each = 16),
-                   stringsAsFactors = F)
+#edges = data.frame(N1 = paste0(rep(LETTERS[1:4], each = 4), rep(1:5, each = 16)),
+#                   N2 = paste0(rep(LETTERS[1:4], 4), rep(2:6, each = 16)),
+#                   Value = runif(80, min = 2, max = 5) * rep(c(1, 0.8, 0.6, 0.4, 0.3), each = 16),
+#                   stringsAsFactors = F)
 
-edges = edges[sample(c(TRUE, FALSE), nrow(edges), replace = TRUE, prob = c(0.8, 0.2)),]
-head(edges)
+#edges = edges[sample(c(TRUE, FALSE), nrow(edges), replace = TRUE, prob = c(0.8, 0.2)),]
+#head(edges)
 
 
 
 ####################################################################
+#' A function to plot flows in R.
+#' This is a slight improvement over the RiverPlot package with slightly better display settings
+#' @param edges a matrix of format:  N1 , N2 , Value
+#' @keywords RiverPlot
+#' @export
+#' @examples
+#' edges = data.frame(N1 = paste0(rep(LETTERS[1:4], each = 4), rep(1:5, each = 16)),
+#' N2 = paste0(rep(LETTERS[1:4], 4), rep(2:6, each = 16)),
+#' Value = runif(80, min = 2, max = 5) * rep(c(1, 0.8, 0.6, 0.4, 0.3), each = 16),
+#' stringsAsFactors = F)
+#' plotflows(edges)
+
+
+
 # take a matrix of format:
 # N1, N2 , Value
 # N1 & N2 represent the transition 
@@ -188,5 +202,5 @@ plotflows <- function( edges ){
 }
 
 
-plotflows(edges)
+#plotflows(edges)
 
